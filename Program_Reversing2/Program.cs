e6b0ff04d55a43a74ed7fa48d9775e97b3505305
NaynaPriyadarshi <you@example.com>
2019-02-20 12:12:11 +1000

    first commit

---
+++ Program_Reversing2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program_Reversing2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string str;
+            int l = 0;
+
+            Console.WriteLine("\n\nprint individual characters of string in reverse order :\n");
+            Console.WriteLine("------------------------------------------------------\n");
+            Console.WriteLine("Input the string : ");
+            str = Console.ReadLine();
+
+            l = str.Length - 1;
+            Console.WriteLine("The characters of the string in reverse are : \n");
+            while (l >= 0)
+            {
+                Console.WriteLine("{0} ", str[l]);
+                l--;
+            }
+            Console.WriteLine("\n\n");
+            Console.ReadLine();
+
+
+        }
+
+    }
+}
